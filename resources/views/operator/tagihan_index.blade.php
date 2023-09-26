b061b2a928af664094b732bc5f1deb5e271ea9ae
avrians <avriansyahb10@gmail.com>
2023-09-26 19:59:44 +0700

    [Av][44][7.50]

--- resources/views/operator/tagihan_index.blade.php
+++ resources/views/operator/tagihan_index.blade.php
@@ -7,64 +7,75 @@
                 <h5 class="card-header">{{ $title }}</h5>
 
                 <div class="card-body">
-                    <a href="{{ route($routePrefix . '.create') }}" class="btn btn-primary btn-sm">Tambah Data</a>
-                    {!! Form::open(['route' => $routePrefix . '.index', 'method' => 'GET']) !!}
-                    <div class="input-group">
-                        <input name="q" type="text" class="form-control" placeholder="Cari Data"
-                            aria-label="Cari Data" aria-describedby="button-addon2" value="{{ request('q') }}">
-                        <button class="btn btn-outline-primary" type="submit" id="button-addon2">
-                            <i class="bx bx-search"></i>
-                        </button>
-                    </div>
-                    {!! Form::close() !!}
-                    <div class="table-responsive">
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th>NISN</th>
-                                    <th>Nama Siswa </th>
-                                    <th>Tanggal Tagihan</th>
-                                    <th>Status</th>
-                                    <th>Created By</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @forelse ($models as $item)
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="{{ route($routePrefix . '.create') }}" class="btn btn-primary btn-sm">Tambah Data</a>
+                        </div>
+
+                        <div class="col-md-6">
+                            {!! Form::open(['route' => $routePrefix . '.index', 'method' => 'GET']) !!}
+                            <div class="row">
+                                <div class="col">
+                                    {!! Form::selectMonth('bulan', request('bulan'), ['class' => 'form-control']) !!}
+                                </div>
+                                <div class="col">
+                                    {!! Form::selectRange('tahun', 2022, date('Y')+1, request('tahun'), ['class' => 'form-control']) !!}
+                                </div>
+                                <div class="col">
+                                    <button class="btn btn-primary" type="submit">Tampil</button>
+                                </div>
+                            </div>
+                            {!! Form::close() !!}
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table table-striped">
+                                <thead>
                                     <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $item->siswa->nisn }}</td>
-                                        <td>{{ $item->siswa->nama }}</td>
-                                        <td>{{ $item->tanggal_tagihan }}</td>
-                                        <td>{{ $item->status }}</td>
-                                        <td>
-                                            {!! Form::open([
-                                                'route' => [$routePrefix . '.destroy', $item->id],
-                                                'method' => 'delete',
-                                                'onsubmit' => 'return confirm(\'Apakah Anda Yakin Ingin Menghapus Data Ini?\')',
-                                            ]) !!}
-                                            <a href="{{ route($routePrefix . '.edit', $item->id) }}"
-                                                class="btn btn-sm btn-warning">
-                                                <i class="fa fa-edit"></i> Edit
-                                            </a>
-                                            <a href="{{ route($routePrefix . '.show', $item->id) }}"
-                                                class="btn btn-sm btn-info">
-                                                <i class="fa fa-edit"></i> Detail
-                                            </a>
-                                            <button type="submit" class="btn btn-sm btn-danger">
-                                                <i class="fa fa-trash"></i> Hapus
-                                            </button>
-                                            {!! Form::close() !!}
-                                        </td>
+                                        <th>No</th>
+                                        <th>NISN</th>
+                                        <th>Nama Siswa </th>
+                                        <th>Tanggal Tagihan</th>
+                                        <th>Status</th>
+                                        <th>Created By</th>
                                     </tr>
-                                @empty
-                                    <tr>
-                                        <td colspan="4">Data Tidak ada</td>
-                                    </tr>
-                                @endforelse
-                            </tbody>
-                        </table>
-                        {!! $models->links() !!}
+                                </thead>
+                                <tbody>
+                                    @forelse ($models as $item)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $item->siswa->nisn }}</td>
+                                            <td>{{ $item->siswa->nama }}</td>
+                                            <td>{{ $item->tanggal_tagihan }}</td>
+                                            <td>{{ $item->status }}</td>
+                                            <td>
+                                                {!! Form::open([
+                                                    'route' => [$routePrefix . '.destroy', $item->id],
+                                                    'method' => 'delete',
+                                                    'onsubmit' => 'return confirm(\'Apakah Anda Yakin Ingin Menghapus Data Ini?\')',
+                                                ]) !!}
+                                                <a href="{{ route($routePrefix . '.edit', $item->id) }}"
+                                                    class="btn btn-sm btn-warning">
+                                                    <i class="fa fa-edit"></i> Edit
+                                                </a>
+                                                <a href="{{ route($routePrefix . '.show', $item->id) }}"
+                                                    class="btn btn-sm btn-info">
+                                                    <i class="fa fa-edit"></i> Detail
+                                                </a>
+                                                <button type="submit" class="btn btn-sm btn-danger">
+                                                    <i class="fa fa-trash"></i> Hapus
+                                                </button>
+                                                {!! Form::close() !!}
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td colspan="4">Data Tidak ada</td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                            {!! $models->links() !!}
+                        </div>
                     </div>
                 </div>
             </div>
