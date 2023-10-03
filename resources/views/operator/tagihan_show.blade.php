9b53c7e2fb992e8aebafb28caa9b220d0be8d848
avrians <avriansyahb10@gmail.com>
2023-10-03 10:56:09 +0700

    [Av][48][8.00]

--- resources/views/operator/tagihan_show.blade.php
+++ resources/views/operator/tagihan_show.blade.php
@@ -7,10 +7,17 @@
                 <h5 class="card-header">DATA SISWA</h5>
 
                 <div class="card-body">
-                    <table>
+                    <table class="table table-sm">
                         <tr>
-                            <td>NISN</td>
-                            <td>:{{ $siswa->nisn }}</td>
+                            <td rowspan="8" width="100"><img src="{{ \Storage::url($siswa->foto) }}" alt="{{ $siswa->nama }}" height="150"></td>
+                        </tr>
+                        <tr>
+                            <td width="50">NISN</td>
+                            <td>: {{ $siswa->nisn }}</td>
+                        </tr>
+                        <tr>
+                            <td width="50">NAMA</td>
+                            <td>: {{ $siswa->nama }}</td>
                         </tr>
                       </table>
                 </div>
